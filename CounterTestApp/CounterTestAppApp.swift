05dfc29f1c14e243b82fef72911555d20e52c573
Dmytro Pryshlyak <salokpi@gmail.com>
2026-07-28 17:51:07 +0300

    Initial Commit

---
+++ CounterTestApp/CounterTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTestAppApp.swift
+//  CounterTestApp
+//
+//  Created by Дмитро on 28.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct CounterTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
